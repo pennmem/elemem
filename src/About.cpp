262c030122146a83834c8e563d91779778cc4467
Ryan A. Colyer <ryancolyer@yahoo.com>
2020-02-07 14:55:25 -0500

    Add edflib license

--- src/About.cpp
+++ src/About.cpp
@@ -7,8 +7,9 @@
 using namespace RC;
 
 namespace CML {
-  // For the JSON library:
-  const RC::RStr LICENSE_MIT {
+  // For the Lohmann JSON library:
+  const RC::RStr LICENSE_JSON {
+    "<p>JSON for Modern C++ library component:</p>"
     "<p>MIT License</p>"
     "<p>Copyright (c) 2013-2019 Niels Lohmann</p>"
     "<p>Permission is hereby granted, free of charge, to any person obtaining "
@@ -29,6 +30,38 @@ namespace CML {
     "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>"
   };
 
+	// For the EDFLib library
+  const RC::RStr LICENSE_EDFLIB {
+    "<p>EDFLib library component:</p>"
+    "<p>BSD 3-Clause License</p>"
+    "<p>Copyright (c) 2009 - 2019 Teunis van Beelen<br/>"
+    "All rights reserved.</p>"
+    "<p>Redistribution and use in source and binary forms, with or without"
+    "modification, are permitted provided that the following conditions are"
+    "met:<p>"
+    "<ul><li>Redistributions of source code must retain the above copyright"
+    "notice, this list of conditions and the following disclaimer.</li>"
+    "<li>Redistributions in binary form must reproduce the above copyright"
+    "notice, this list of conditions and the following disclaimer in the"
+    "documentation and/or other materials provided with the"
+    "distribution.</li>"
+    "<li>Neither the name of the copyright holder nor the names of its"
+    "contributors may be used to endorse or promote products derived from"
+    "this software without specific prior written permission.</li></ul>"
+    "<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS"
+    "\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT"
+    "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A"
+    "PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT"
+    "HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,"
+    "SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT"
+    "LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,"
+    "DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY"
+    "THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT"
+    "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE"
+    "OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH"
+    "DAMAGE.</p>"
+  };
+
   void AboutWin() {
     RStr year = RStr(MYTIMESTAMP).SplitWords().Last();
 
@@ -46,8 +79,8 @@ namespace CML {
               + "<p><a href=\"https://memory.psych.upenn.edu\">https://memory.psych.upenn.edu</a></p>";
     text += "<i>";
     text += "<hr/>";
-    text += "<p>JSON for Modern C++ library component:</p>";
-    text += LICENSE_MIT;
+    text += LICENSE_JSON;
+		text += LICENSE_EDFLIB;
     text += "</i>";
 
     QMessageBox::about(0, title.c_str(), text.c_str());
