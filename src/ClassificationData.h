c0cf890dc27dbe0765b4612abc63512deb79d1f8
James Bruska <jamesbthecoder@gmail.com>
2021-10-19 18:53:09 -0400

    Got first version of MorletTransformer running on a callback of ClassificationData the data binning class (EEGAcq->ClassificationData->MorletTransformer)

--- src/ClassificationData.h
+++ src/ClassificationData.h
@@ -11,13 +11,20 @@ namespace CML {
   class Handler;
   class Classifier;
 
+  //using EEGCallback = RC::Caller<void, RC::APtr<const EEGData>>;
+
   class ClassificationData : public RCqt::WorkerThread {
     public:
-
     ClassificationData(RC::Ptr<Handler> hndl, size_t sampling_rate); 
 
     RCqt::TaskCaller<RC::APtr<const EEGData>> ClassifyData = 
       TaskHandler(ClassificationData::ClassifyData_Handler);
+    
+    void DoNothing(RC::APtr<const EEGData>& data) { RC_DEBOUT(RC::RStr("\n\nSETUP FUNC\n\n")); }
+    RCqt::TaskCaller<RC::APtr<const EEGData>> callback = TaskHandler(ClassificationData::DoNothing);
+
+    //RCqt::TaskCaller<EEGCallback> SetCallback = 
+    //  TaskHandler(ClassificationData::SetCallback_Handler);
 
     protected:
     // TODO: Decide whether to have json configurable variables for the
@@ -31,10 +38,14 @@ namespace CML {
     //void StopClassifier_Handler() override;
     void ClassifyData_Handler(RC::APtr<const EEGData>& data);
 
+    //void SetCallback_Handler(EEGCallback& new_callback);
+
     RC::Ptr<Handler> hndl;
     EEGData buffer;
     size_t sampling_rate;
     RC::RStr callback_ID;
+
+    //EEGCallback callback;
 
     u32 dataSinceLastClassification;
     RC::APtr<Classifier> classifier;
