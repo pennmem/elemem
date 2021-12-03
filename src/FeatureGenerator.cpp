5c067f0264ac1d91d3e9e49eb7ad813cdf78adca
James Bruska <jamesbthecoder@gmail.com>
2021-12-03 15:43:56 -0500

    Made all SetCallback functions take a const function

--- src/FeatureGenerator.cpp
+++ src/FeatureGenerator.cpp
@@ -4,7 +4,7 @@ namespace CML {
   /// Handler that sets the callback on the feature generator results
   /** @param The callback on the classifier results
    */
-  void FeatureGenerator::SetCallback_Handler(FeatureCallback &new_callback) {
+  void FeatureGenerator::SetCallback_Handler(const FeatureCallback &new_callback) {
     callback = new_callback;  
   }  
 }
