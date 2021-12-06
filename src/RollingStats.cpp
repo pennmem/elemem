a722e73315048b3da293132bf31328b5e6364702
James Bruska <jamesbthecoder@gmail.com>
2021-12-06 14:55:55 -0500

    Added documentation, improved code with RC macros, renamed some things, and added a array length check

--- src/RollingStats.cpp
+++ src/RollingStats.cpp
@@ -1,40 +1,47 @@
 #include "RollingStats.h"
 
 namespace CML {
-  RollingStats::RollingStats(int num_feats) {
-    count = 0;
-    means.Resize(num_feats);
-    m2s.Resize(num_feats);
+  /// Default constructor that initializes and resets the internal lists
+  /** @param the number of values in each list
+   */
+  RollingStats::RollingStats(int num_values) {
+    means.Resize(num_values);
+    m2s.Resize(num_values);
     Reset();
   }
 
+  /// Reset all of the values back to 0 
   void RollingStats::Reset() {
-    for (double &mean : means) 
-      mean = 0;
-    for (double &m2 : m2s)
-      m2 = 0;
+    count = 0;
+    RC_ForEach(mean, means) { mean = 0; }
+    RC_ForEach(m2, m2s) { m2 = 0; }
   }
 
-  void RollingStats::Update(RC::Data1D<double> new_feats) {
-    // TODO: JPB: Force new_feats to be same size as means & m2s
+  /// Update the rolling statistics with a new set of values
+  /** @param The new values to be added to the rolling statics
+   */
+  void RollingStats::Update(RC::Data1D<double> new_values) {
+    if (new_values.size() != means.size())
+      Throw_RC_Type(Bounds, "Data1D size mismatch between new_values and means/m2s");
     count += 1;
-    for (size_t i=0; i<new_feats.size(); ++i) {
-      double delta = new_feats[i] - means[i];
+    RC_ForIndex(i, new_values) {
+      double delta = new_values[i] - means[i];
       means[i] += delta / count;
-      double delta2 = new_feats[i] - means[i];
+      double delta2 = new_values[i] - means[i];
       m2s[i] += delta * delta2;
     }
   }
-  
+
+  /// Returns the current statistics from the collected data
+  /** @return The current statistics
+   */
   StatsData RollingStats::GetStats() {
     RC::Data1D<double> std_dev(m2s.size());
     RC::Data1D<double> sample_std_dev(m2s.size());
-    for (size_t i=0; i<m2s.size(); ++i) {
+    RC_ForIndex(i, m2s) {
       std_dev[i] = m2s[i] / count;
       sample_std_dev[i] = m2s[i] / (count - 1);
     }
-    
-    StatsData ret {means, std_dev, sample_std_dev};
-    return ret;
+    return StatsData {means, std_dev, sample_std_dev};
   }
 }
--- src/RollingStats.h
+++ src/RollingStats.h
@@ -9,15 +9,18 @@ namespace CML {
     RC::Data1D<double> means = {};
     RC::Data1D<double> std_devs = {};
     RC::Data1D<double> sample_std_devs = {};
-    StatsData(size_t num_feats) : means(num_feats), std_devs(num_feats), sample_std_devs(num_feats) {}
-    StatsData(RC::Data1D<double> means, RC::Data1D<double> std_devs, RC::Data1D<double> sample_std_devs) : means(means), std_devs(std_devs), sample_std_devs(sample_std_devs) {}
+    StatsData(size_t num_values)
+      : means(num_values), std_devs(num_values), sample_std_devs(num_values) {}
+    StatsData(RC::Data1D<double> means, RC::Data1D<double> std_devs, 
+              RC::Data1D<double> sample_std_devs)
+      : means(means), std_devs(std_devs), sample_std_devs(sample_std_devs) {}
   };
 
   class RollingStats {
     public:
-    RollingStats(int num_feats);
+    RollingStats(int num_values);
 
-    void Update(RC::Data1D<double> new_feats);
+    void Update(RC::Data1D<double> new_values);
     void Reset();
     StatsData GetStats();
 
