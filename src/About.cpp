7fc1b6b44d8e5b7d9b43f37dfbf464441be4e650
Ryan A. Colyer <ryancolyer@yahoo.com>
2024-04-22 13:03:45 -0400

    Add DSPFilters license note

--- src/About.cpp
+++ src/About.cpp
@@ -65,6 +65,30 @@ namespace CML {
     "DAMAGE.</p>"
   };
 
+  const RC::RStr LICENSE_DSPFilters {
+    "<p>DSPFilters component:</p>"
+    "<p>License: MIT License "
+    "(http://www.opensource.org/licenses/mit-license.php)</p>"
+    "<p>Copyright (c) 2009 by Vinnie Falco</p>"
+    "<p>Permission is hereby granted, free of charge, to any person "
+    "obtaining a copy of this software and associated documentation files "
+    "(the \"Software\"), to deal in the Software without restriction, "
+    "including without limitation the rights to use, copy, modify, merge, "
+    "publish, distribute, sublicense, and/or sell copies of the Software, "
+    "and to permit persons to whom the Software is furnished to do so, "
+    "subject to the following conditions:</p>"
+    "<p>The above copyright notice and this permission notice shall be "
+    "included in all copies or substantial portions of the Software.</p>"
+    "<p>THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, "
+    "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF "
+    "MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. "
+    "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY "
+    "CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, "
+    "TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE "
+    "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>"
+  };
+
+
   void AboutWin() {
     RStr year = ElememVersion().SplitWords().Last();
 
@@ -84,6 +108,7 @@ namespace CML {
     text += "<hr/>";
     text += LICENSE_JSON;
     text += LICENSE_EDFLIB;
+    text += LICENSE_DSPFilters;
     text += "</i>";
 
     QMessageBox::about(0, title.c_str(), text.c_str());
